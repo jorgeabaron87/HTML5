e03200784b82dcb4c41ee09709e6dacc71ca8dc2
jorge <jorgeabaron87@gmail.com>
2014-10-31 14:53:10 +0100

    Capitulo 2
    Cargar Niveles

--- Capitulo2/js/game.js
+++ Capitulo2/js/game.js
@@ -8,6 +8,7 @@ var game = {
 
 		//Initialize objects
 		levels.init();
+		loader.init();
 
 
 		//Hide all game layers and display the start screen
@@ -59,7 +60,92 @@ var levels = {
 	//Load all data and images for a specific level
 	load: function(number){
 
+		//declare a new currentLevel object
+		game.currentLevel = {number:number,hero:[]};
+		game.score = 0;
+
+		$('#score').html('Score:' + game.score);
+		var level = levels.data[number];
+
+		// load the background, foreground,, and slingshot images
+		game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/"+level.background + ".png");
+		game.currentLevel.foregroundImage = loader.loadImage("images/backgrounds/"+level.foreground + ".png");
+		game.slingshotImage = loader.loadImage("images/slingshot.png");
+		game.slingshotFrontImage=loader.loadImage("images/slingshot-front.png");
+
+		// Call game.start() once the assets have loaded
+
+		if(loader.loaded){
+			game.start();
+		}else{
+			loader.onload = game.start;
+		}
+
 	}
 
+
+}
+
+var loader={
+	loaded:true,
+	loadedCount:0, // Asset that have been loaded so far
+	totalCount:0, // Total number of asset that need to be loaded
+
+	init:function(){
+		// check for sound support
+		var mp3Support,oggSupport;
+		var audio = document.createElement('audio');
+		if (audio.canPlayType) {
+			// Currently canPlayType() returns: "", "maybe" or "probably"
+			mp3Support = "" != audio.canPlayType('audio/mpeg');
+			oggSupport = "" != audio.canPlayType('audio/ogg; codecs = "vorbis"');
+		} else {
+			//The audio tag is not supported
+			mp3Support = false;
+			oggSupport = false;
+		}
+		
+		// Check for ogg, then mp3, and finally set soundFileExtn to undefined
+		loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;
+	},
+
+	loadImage:function(url){
+		this.totalCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var image = new Image();
+		image.src = url;
+		image.onload = loader.itemLoaded;
+		return image;
+	},
+
+	soundFileExtn:".ogg",
+	
+	loadSound:function(url){
+		this.totalCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var audio = new Audio();
+		audio.src = url + loader.soundFileExtn;
+		audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+		return audio;
+	},
+	
+	itemLoaded:function(){
+		loader.loadedCount++;
+		$('loadingmessage').html('Loaded' +loader.loadedCount+' of '+ loader.totalCount);
+		if(loader.loadedCount = loader.totalCount){
+			// Loader has loaded completely..
+			loader.loaded=true;
+			//Hide the loading screen
+			$('loadingscreen').hide();
+			// and call the loader.onload method if it exists
+
+			if(loader.onload){
+				loader.onload();
+				loader.onload = undefined;
+			}
+		}
+	},
 
 }
